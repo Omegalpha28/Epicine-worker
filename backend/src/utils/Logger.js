628ce9acd69faa0ea5dd32af4afc3db868d70d98
lagie-marin <marin.lagie@epitech.eu>
2024-09-18 14:46:31 +0200

    [UPDATE]modify space in file

--- backend/src/utils/Logger.js
+++ backend/src/utils/Logger.js
@@ -4,29 +4,29 @@ const dotenv = require("dotenv");
 const format = "{tstamp} {tag} {txt}\n";
 
 async function error(content) {
-  await write(content, "black", "bgRed", "ERROR", true);
+    await write(content, "black", "bgRed", "ERROR", true);
 }
 
 async function serveur(content) {
-  await write(content, "black", "bgBlue", "SERVER", false);
+    await write(content, "black", "bgBlue", "SERVER", false);
 }
 
 async function logs(content) {
-  await write(content, "black", "bgWhite", "LOG", false);
+    await write(content, "black", "bgWhite", "LOG", false);
 }
 
 async function write(content, tagColor, bgTagColor, tag, error = false) {
-  const chalk = (await import("chalk")).default;
-  const timestamp = `[${dayjs().format("DD/MM - HH:mm:ss")}]`;
-  const logTag = `[${tag}]`;
-  const stream = error ? process.stderr : process.stdout;
+    const chalk = (await import("chalk")).default;
+    const timestamp = `[${dayjs().format("DD/MM - HH:mm:ss")}]`;
+    const logTag = `[${tag}]`;
+    const stream = error ? process.stderr : process.stdout;
 
-  const item = format
-    .replace("tstamp", chalk.gray(timestamp))
-    .replace("{tag}", chalk[bgTagColor][tagColor](logTag))
-    .replace("{txt}", chalk.white(content));
-  appendToFile(item);
-  stream.write(item);
+    const item = format
+        .replace("tstamp", chalk.gray(timestamp))
+        .replace("{tag}", chalk[bgTagColor][tagColor](logTag))
+        .replace("{txt}", chalk.white(content));
+    appendToFile(item);
+    stream.write(item);
 }
 
 module.exports = { error, serveur, logs };
