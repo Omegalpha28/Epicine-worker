abff6bc2d8e02e340f1f826a57bdabb789a850f6
lagie-marin <marin.lagie@epitech.eu>
2024-09-19 16:53:56 +0200

    UPDATE: change type user id: string=>int and add createUser and updateUser function

--- backend/src/core/data/models/user.js
+++ backend/src/core/data/models/user.js
@@ -2,8 +2,9 @@ const { Schema, Model } = require("../../bdd/sql-connector");
 
 const userSchema = new Schema({
     id: {
-        type: String,
-        unique: true
+        type: Number,
+        unique: true,
+        auto_increment: true
     },
     name: String,
     email: String,
