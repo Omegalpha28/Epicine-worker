01b4decf18df8b2014ec8dc578457978d8923aec
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-20 17:13:58 +0200

    ADD: adding tv show

---
+++ frontend/src/components/TV_Shows/TV_Shows.jsx
@@ -0,0 +1,12 @@
+import "./TV_Shows.module.css";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "../../App.module.css";
+
+export const TV_Shows = () => {
+
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    );
+};
