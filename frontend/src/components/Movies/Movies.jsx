f56f238c18fd2b034c04f589d09a773c495e8d3c
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-20 17:12:43 +0200

    ADD: ading new page

---
+++ frontend/src/components/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import "./Movies.module.css";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "../../App.module.css";
+
+export const Movies = () => {
+
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    );
+};
