5e3c82444e0ab25cb7170481f8f49671448e1d91
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-20 17:14:56 +0200

    ADD: adding new pages

---
+++ frontend/src/components/Trailers/Trailers.jsx
@@ -0,0 +1,12 @@
+import "./Trailers.module.css";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "../../App.module.css";
+
+export const TrailersPage = () => {
+
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    );
+};
