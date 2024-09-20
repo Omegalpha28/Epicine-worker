c94f9170a64199d400885642c1196a24030736bd
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-20 17:11:40 +0200

    ADD: adding new release page

---
+++ frontend/src/components/NewRelease/NewRelease.jsx
@@ -0,0 +1,12 @@
+import "./NewRelease.module.css";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "../../App.module.css";
+
+export const NewReleasePage = () => {
+
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    );
+};
