74a4007a7c9890a9ba4778e49fd0e39a88a11368
lagie-marin <marin.lagie@epitech.eu>
2024-09-21 00:12:44 +0200

    UPDATE: insert user with name, email, password (hash). Check if email is unique.

--- backend/src/middleware/notFound.js
+++ backend/src/middleware/notFound.js
@@ -1,7 +1,17 @@
+const { client } = require("../core/bdd/sql-connector");
+const { getUser } = require("../core/data/config.function");
+
 module.exports = (req, res, next) => {
     var id = req.params.id;
 
     if (id) {
-        
+        const userData = getUser(client, id);
+
+        if (userData == undefined || userData.length == 0)
+            res.status(404).json({"msg": "Bad parameter"});
+        else
+            next();
     }
+    else
+        res.status(500).json({ "msg": "Internal server error" })
 }
--- backend/src/routes/auth/auth.js
+++ backend/src/routes/auth/auth.js
@@ -1,3 +1,21 @@
-module.exports = function (client, app, bcrypt) {
-    app.get("/user", )
+const { logs } = require("../../utils/Logger");
+const { validUserArgs } = require("../../utils/valid_args");
+const { checkAccountMail, register } = require("../user/user.query");
+
+module.exports = async function (client, app, bcrypt) {
+    app.post("/register", (req, res) => {
+        var email = req.body["email"];
+        var name = req.body["name"];
+        var mdp = req.body["password"];
+
+        if (validUserArgs(res, email, name, mdp)) {
+            mdp = bcrypt.hashSync(mdp, 10);
+            checkAccountMail(client, res, email, async nb => {
+                if (nb == 84)
+                    res.status(409).json({ "msg": "Account already exists" });
+                else
+                    await register(client, res, email, name, mdp);
+            })
+        }
+    })
 }
