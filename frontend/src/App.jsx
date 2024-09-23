153f1b962311e6a17862b5c2660f6807612ab8d8
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-23 10:29:31 +0200

    [UPDATE]: adding some routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import useLocalStorage from "use-local-storage";
 import { HomePage } from "./components/Home/HomePage";
-import { LoginPage } from "./components/Account/Account";
+import { LoginPage } from "./components/Account/LogIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NewReleasePage } from "./components/NewRelease/NewRelease";
 import { Movies } from "./components/Movies/Movies";
 import { TV_Shows } from "./components/TV_Shows/TV_Shows";
 import { Streaming } from "./components/Streaming/Streaming";
+import { SignUpPage } from "./components/Account/SignUp";
 import { TrailersPage } from "./components/Trailers/Trailers";
 
 function App() {
@@ -17,12 +18,13 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/Account" element={<LoginPage />} />
+                <Route path="/LogIn" element={<LoginPage />} />
                 <Route path="/New_Release" element={<NewReleasePage />} />
                 <Route path="/Movies" element={<Movies />} />
                 <Route path="/TV_Shows" element={<TV_Shows />} />
                 <Route path="/Streaming" element={<Streaming />} />
                 <Route path="/Trailers" element={<TrailersPage />} />
+                <Route path="/SignUp" element={<SignUpPage />} />
             </Routes>
         </Router>
     </div>
