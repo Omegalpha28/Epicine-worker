b228460f929851975af26dec5e5cd73ed6f06104
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-25 13:29:17 +0200

    [UPDATE]: fixing theme mode

--- frontend/src/components/NewRelease/NewRelease.jsx
+++ frontend/src/components/NewRelease/NewRelease.jsx
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from "react";
+import useTheme from "../set_theme";
 import styles from "./NewRelease.module.css";
-import useLocalStorage from "use-local-storage";
 import { Navbar } from "../Navbar/Navbar";
 import { Toggle } from "../Toggle/Toggle";
 
 export const NewReleasePage = () => {
-  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
-  const [firstTime, setFirstTime] = useState(0);
 
-  useEffect(() => {
-    if (preference && firstTime === 0) {
-      setIsDark(true);
-      setFirstTime(1);
-    }
-  }, [preference, firstTime, setIsDark]);
+  const [isDark, setIsDark] = useTheme();
 
   return (
     <div className={styles.New_Release_nav} data-theme={isDark ? "dark" : "light"}>
       <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
       <Navbar />
-      <div className={styles.New_Release_page} data-theme={isDark ? "dark" : "light"}>
+      <div className={styles.New_Release_page}>
         <h1>ok boomer</h1>
       </div>
     </div>
