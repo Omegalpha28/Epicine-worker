51471630f6d6b895587887b0bf2daefe1dc009ab
ossanomegalpha <ossan.msoili@epitech.eu>
2024-09-25 17:58:22 +0200

    [FIX]: fixing theme

--- frontend/src/components/Movies/Movies.jsx
+++ frontend/src/components/Movies/Movies.jsx
@@ -1,15 +1,34 @@
-import "./Movies.module.css";
-import { Navbar } from "../Navbar/Navbar";
-import styles from "../../App.module.css";
 import useTheme from "../set_theme";
+import styles from "./Movies.module.css";
+import { Navbar } from "../Navbar/Navbar";
+import { Toggle } from "../Toggle/Toggle";
+import app_styles from "../../App.module.css";
+import { Join_Us } from "../Joinus/join_us";
 
 export const Movies = () => {
 
-    const [isDark, setIsDark] = useTheme();
+  const [isDark, setIsDark] = useTheme();
 
-    return (
-        <div className={styles.App} data-theme={isDark ? "dark" : "light"}>
-            <Navbar />
+  return (
+    <div className={styles.page} data-theme={isDark ? "dark" : "light"}>
+      <div className={app_styles.App} >
+        <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+        <Navbar />
+        <div className={styles.New_Release_page}>
+          <nav className={styles.header}>
+            <div className={styles.searchContainer}>
+                <input type="text" placeholder="Type to search..." className={styles.searchInput} />
+                <button className={styles.searchButton}>
+                  Search
+                </button>
+            </div>
+          </nav>
         </div>
-    );
-};
+      <div className={styles.inside_box}>
+          <h1>Movies</h1>
+      </div>
+    </div>
+    <Join_Us />
+    </div>
+  );
+}
