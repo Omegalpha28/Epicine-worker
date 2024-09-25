306ee26f27e026ce1f1edcacab424d4cfe5394dc
lagie-marin <marin.lagie@epitech.eu>
2024-09-25 15:58:45 +0200

    [UPDATE]: Added security

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      // Options de l'obfuscateur
+      compact: true,
+      controlFlowFlattening: true,
+      deadCodeInjection: true,
+      debugProtection: true,
+      disableConsoleOutput: true,
+      rotateStringArray: true,
+      stringArray: true,
+      stringArrayEncoding: ['base64'], // Encodage en base64 pour les chaînes
+      stringArrayThreshold: 0.75
+    })
+  ],
+  build: {
+    sourcemap: false // Ne pas inclure de source maps pour plus de sécurité
+  },
   css: {
     modules: {
       localsConvention: "camelCase",
     }
   }
-})
+});
