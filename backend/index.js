78199a87f4ec4c86ab1fb203f0821b176943a294
lagie-marin <marin.lagie@epitech.eu>
2024-09-26 11:24:57 +0200

    [FIX]: backend run in Epicine

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ main().then(() => {
     require("./src/routes/user/user")(client, app, bcrypt);
     app.listen(port, () => {
         Logger.logs(`Listening at port: ${port}`);
-        Logger.serveur(`EpiTodo server: http://localhost:${port}`);
+        Logger.serveur(`EpiCine server: http://localhost:${port}`);
     });
 });
 
