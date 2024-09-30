22e7b57567abf5a78e77227319e0a4aac6440329
Saravenpi Shigeturi <saravepi@tuta.io>
2024-09-30 16:57:37 +0200

    [ADD] latest release movie, box free change by latest release and latest release by upcoming

--- backend/src/routes/list/list_tv.js
+++ backend/src/routes/list/list_tv.js
@@ -76,17 +76,12 @@ module.exports = (client, app, bcrypt) => {
         }
     });
 
-
-    async function getLatestReleases() {
-        const discoverUrl = `https://api.themoviedb.org/3/discover/movie?language=en-FR`;
+    async function getLatestReleases(type) {
+        const discoverUrl = `https://api.themoviedb.org/3/discover/${type}?language=en-FR`;
         const releaseDatesMap = {};
 
-        console.log(`____________________________________________________`);
-        console.log(`Début de la récupération des films...`);
-        console.log(`URL de découverte : ${discoverUrl}`);
-        console.log(`____________________________________________________`);
-
         try {
+            // Récupérer les films ou les séries
             const response = await fetch(discoverUrl, {
                 method: 'GET',
                 headers: {
@@ -96,123 +91,94 @@ module.exports = (client, app, bcrypt) => {
             });
 
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                throw new Error(`Erreur lors de la récupération des ${type === 'movie' ? 'films' : 'séries'} : ${response.status}`);
             }
 
-            const { results } = await response.json();
-            console.log(`Nombre de films récupérés : ${results.length}`);
-            console.log(`____________________________________________________`);
+            const results = await response.json();
 
             // Obtenir la date limite pour les 30 derniers jours
             const thirtyDaysAgo = new Date();
-            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-            console.log(`Date limite pour les 30 derniers jours : ${thirtyDaysAgo.toISOString().split('T')[0]}`);
-            console.log(`____________________________________________________`);
+            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 365); // Use 30 days instead of 365 for the latest releases check
 
-            // Parcourir tous les films et récupérer leurs dates de sortie
-            for (const movie of results) {
-                console.log(`Récupération des dates de sortie pour le film : ${movie.title} (ID : ${movie.id})`);
-                const releaseUrl = `https://api.themoviedb.org/3/movie/${movie.id}/release_dates?language=en-FR`;
-                console.log(`URL des dates de sortie : ${releaseUrl}`);
+            // Fonction pour récupérer les dates de sortie
+            const getReleaseDates = async (results) => {
+                for (const item of results) {
+                    const releaseUrl = `https://api.themoviedb.org/3/${type}/${item.id}/release_dates?language=en-FR`;
 
-                const releaseResponse = await fetch(releaseUrl, {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: `Bearer ${TMDB_API_KEY}`,
-                    },
-                });
-
-                if (!releaseResponse.ok) {
-                    console.error(`Erreur lors de la récupération des dates de sortie pour ${movie.title}: HTTP error! status: ${releaseResponse.status}`);
-                    continue; // Continuer avec le prochain film
-                }
-
-                const releaseData = await releaseResponse.json();
-                console.log(`Réponse reçue pour ${movie.title}:`, releaseData);
-                console.log(`____________________________________________________`);
-
-                if (releaseData && releaseData.results) {
-                    let foundReleaseDate = false;
-
-                    // Afficher les pays disponibles et leurs dates de sortie
-                    console.log(`Analyse des dates de sortie pour ${movie.title}:`);
-                    releaseData.results.forEach((entry) => {
-                        console.log(`  - Pays : ${entry.iso_3166_1}, Dates de sortie disponibles : ${entry.release_dates.length}`);
-
-                        entry.release_dates.forEach(releaseDateEntry => {
-                            const releaseDateStr = releaseDateEntry.release_date;
-                            console.log(`    - Date de sortie trouvée : ${releaseDateStr}`);
-
-                            // Vérifier que la date est valide
-                            const releaseDateObj = new Date(releaseDateStr);
-                            if (!isNaN(releaseDateObj.getTime())) {
-                                // Format de la date en YYYY-MM-DD
-                                const formattedReleaseDate = releaseDateObj.toISOString().split('T')[0];
-
-                                // Vérifier si la date de sortie est dans les 30 derniers jours ou dans le futur
-                                if (releaseDateObj >= thirtyDaysAgo && !foundReleaseDate) {
-                                    // Prendre seulement la première date trouvée pour éviter les doublons
-                                    releaseDatesMap[movie.id] = {
-                                        movieId: movie.id,
-                                        title: movie.title,
-                                        releaseDate: formattedReleaseDate, // Utiliser le format YYYY-MM-DD
-                                        poster_path: movie.poster_path,
-                                    };
-                                    foundReleaseDate = true; // On a trouvé une date valide
-
-                                    // Print des informations sur le film et sa date de sortie
-                                    console.log(`    - Film : ${movie.title}`);
-                                    console.log(`      Date de sortie : ${formattedReleaseDate}`);
-                                    console.log(`      Affiche : https://image.tmdb.org/t/p/w500${movie.poster_path}`);
-                                    console.log(`____________________________________________________`);
-                                } else {
-                                    console.log(`    - Date ${formattedReleaseDate} ne correspond pas aux critères.`);
-                                }
-                            } else {
-                                console.warn(`    - Date invalide pour ${movie.title} : ${releaseDateStr}`);
-                            }
-                        });
+                    const releaseResponse = await fetch(releaseUrl, {
+                        method: 'GET',
+                        headers: {
+                            accept: 'application/json',
+                            Authorization: `Bearer ${TMDB_API_KEY}`,
+                        },
                     });
-                } else {
-                    console.warn(`Aucune date de sortie trouvée pour ${movie.title}`);
+
+                    if (!releaseResponse.ok) {
+                        continue; // Continuer avec le prochain film/série
+                    }
+
+                    const releaseData = await releaseResponse.json();
+
+                    if (releaseData && releaseData.results) {
+                        let foundReleaseDate = false;
+
+                        for (const entry of releaseData.results) {
+                            if (entry.iso_3166_1 === "FR") {
+                                for (const releaseDateEntry of entry.release_dates) {
+                                    // Vérifier que la note est vide
+                                    if (releaseDateEntry.note === "") {
+                                        const releaseDateStr = releaseDateEntry.release_date;
+                                        const releaseDateObj = new Date(releaseDateStr);
+
+                                        // Vérifier si la date de sortie est valide et dans le passé
+                                        if (!isNaN(releaseDateObj.getTime()) && releaseDateObj < new Date()) {
+                                            // Vérifier si la date de sortie est dans les 30 derniers jours
+                                            if (releaseDateObj >= thirtyDaysAgo && !foundReleaseDate) {
+                                                releaseDatesMap[item.id] = {
+                                                    id: item.id,
+                                                    title: item.title || item.name,
+                                                    releaseDate: releaseDateObj.toISOString().split('T')[0],
+                                                    poster_path: item.poster_path,
+                                                };
+                                                foundReleaseDate = true;
+                                                break; // Sortir dès qu'on a trouvé une date de sortie conforme
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-            }
+            };
+
+            // Récupérer les dates de sortie pour les films ou les séries
+            await getReleaseDates(results.results);
 
             // Trier par date de sortie la plus récente
             const releaseDatesArray = Object.values(releaseDatesMap);
-            console.log(`____________________________________________________`);
-            console.log(`Total des dates de sortie récupérées : ${releaseDatesArray.length}`);
-            console.log(`____________________________________________________`);
 
             // Trier par date de sortie de la plus récente à la plus ancienne
             releaseDatesArray.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate));
 
-            // Sélectionner uniquement les 50 films les plus récents
-            const latestReleases = releaseDatesArray.slice(0, 50);
-            console.log(`Films sélectionnés (les 50 plus récents) :`);
-            latestReleases.forEach((movie) => {
-                console.log(`Titre : ${movie.title}, Date de sortie : ${movie.releaseDate}, ID : ${movie.movieId}`);
-            });
-            console.log(`____________________________________________________`);
-
+            // Sélectionner uniquement les 50 films/séries les plus récents
+            const latestReleases = releaseDatesArray.slice(0, 100);
             return latestReleases;
 
         } catch (error) {
-            console.error('Erreur lors de la récupération des données :', error);
             throw new Error('Erreur lors de la récupération des données.');
         }
     }
 
-    // Route pour appeler la fonction via le frontend
-    app.get('/api/latest-releases', async (req, res) => {
+
+    app.get('/api/latest-releases/:type', async (req, res) => {
+        const type = req.params.type === 'movies' ? 'movie' : 'tv'; // 'movies' devient 'movie' et 'series' devient 'tv'
         try {
-            const releases = await getLatestReleases();
+            const releases = await getLatestReleases(type);
             res.json(releases);
         } catch (error) {
             res.status(500).json({ message: 'Erreur lors de la récupération des dernières sorties.' });
         }
     });
-
 };
 
--- frontend/src/components/Home/Latest_Release/Latest_Release.jsx
+++ frontend/src/components/Home/Latest_Release/Latest_Release.jsx
@@ -1,89 +1,53 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import styles from "./Latest_Release.module.css";
 
 export const Latest_Release = () => {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const movieListRef = useRef(null);
 
-    const fetchContent = async (type) => {
-        setLoading(true);
-        setError(null);
-
-        try {
-            const response = await fetch(`http://localhost:5555/api/latest-releases`);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            setMovies(data);
-        } catch (err) {
-            setError("Erreur lors de la récupération des données.");
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchContent('movies');
-    }, []);
-
-    const scrollLeft = () => {
-        movieListRef.current.scrollBy({ left: -200, behavior: "smooth" });
-    };
-
-    const scrollRight = () => {
-        movieListRef.current.scrollBy({ left: 200, behavior: "smooth" });
-    };
-
-    return (
-        <div className={styles.box}>
-            <div className={styles.main_box}>
-                <div className={styles.header}>
-                    <h1 className={styles.title_box}>Latest Release</h1>
-                    <div className={styles.categories}>
-                        <button
-                            className={styles.movies}
-                            onClick={() => fetchContent('movies')}
-                        >
-                            Movies
-                        </button>
-                        <button
-                            className={styles.series}
-                            onClick={() => fetchContent('series')}
-                        >
-                            Series
-                        </button>
-                    </div>
-                </div>
-                <div className={styles.inside_box}>
-                    <div className={styles.carousel_controls}>
-                        <button className={styles.carousel_button} onClick={scrollLeft}>❮</button>
-                        <button className={styles.carousel_button} onClick={scrollRight}>❯</button>
-                    </div>
-                    {loading ? (
-                        <h1>Loading...</h1>
-                    ) : error ? (
-                        <h1>{error}</h1>
-                    ) : movies.length > 0 ? (
-                        <div className={styles.movie_list} ref={movieListRef}>
-                            {movies.map((movie) => (
-                                <div key={movie.movieId} className={styles.movie_item}>
-                                    <img
-                                        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                                        alt={movie.title || "No Title"}
-                                    />
-                                    <h3 className={styles.movie_title}>{movie.title}</h3>
-                                    <p className={styles.release_date}>{movie.releaseDate}</p>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <h1>No Movies Available</h1>
-                    )}
-                </div>
+    return <div className={styles.box}>
+        <div className={styles.main_box}>
+            <h1 className={styles.title_box}>Latest Release</h1>
+            <div className={styles.categories}>
+                <button className={styles.movies}>Movies</button>
+                <button className={styles.series}>Series</button>
+            </div>
+            <div className={styles.inside_box}>
+                <h1>Elements</h1>
             </div>
         </div>
-    );
-};
+    </div>
+}
+
+// return (
+//     <div className={styles.box}>
+//         <div className={styles.main_box}>
+//             <div className={styles.header}>
+//                 <h1 className={styles.title_box}>Latest Release</h1>
+//                 <div className={styles.categories}>
+//                     <button className={styles.movies} onClick={() => fetchContent('movies')}>Movies</button>
+//                     <button className={styles.series} onClick={() => fetchContent('series')}>Series</button>
+//                 </div>
+//             </div>
+//             <div className={styles.inside_box}>
+//                 {loading ? (
+//                     <h1>Loading...</h1>
+//                 ) : error ? (
+//                     <h1>{error}</h1>
+//                 ) : movies.length > 0 ? (
+//                     <div className={styles.movie_list}>
+//                         {movies.map((movie) => (
+//                             <div key={movie.id} className={styles.movie_item}>
+//                                 <img
+//                                     src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+//                                     alt={movie.title || movie.name}
+//                                 />
+//                                 <h3 className={styles.movie_title}>{movie.title || movie.name}</h3>
+//                             </div>
+//                         ))}
+//                     </div>
+//                 ) : (
+//                     <h1>No Movies Available</h1>
+//                 )}
+//             </div>
+//         </div>
+//     </div>
+// );
