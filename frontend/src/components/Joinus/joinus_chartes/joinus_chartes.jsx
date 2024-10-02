5ae082979ee2cc607d7a24e3e354295e9d58bc95
LedernaK <mathis.quenardel@epitech.eu>
2024-10-02 17:35:47 +0200

    [ADD]: Authors and footer logo not responsive

---
+++ frontend/src/components/Joinus/joinus_chartes/joinus_chartes.jsx
@@ -0,0 +1,10 @@
+import styles from "./joinus_chartes.module.css";
+import { getImageUrl } from "../../../utils";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export const Joinus_chartes = () => {
+    return (
+        <></>
+    )
+}
