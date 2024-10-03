4da803bf8feb61902a0846bd433f746ace7f76be
ossanomegalpha <ossan.msoili@epitech.eu>
2024-10-04 01:08:34 +0200

    [FIX]: fixing Recommandations

--- frontend/src/components/MoviePage/Recommendation/Recommendation.jsx
+++ frontend/src/components/MoviePage/Recommendation/Recommendation.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 import styles from './Recommendation.module.css';
 import useTheme from '../../set_theme';
 import { Link } from 'react-router-dom';
+import unknown from '../../../../assets/film-movies-icon.svg';  // Import fallback SVG
 
 export const Recommendation = ({ movieId }) => {
     const [isDark, setIsDark] = useTheme();
     const [recommendations, setRecommendations] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [failedMovies, setFailedMovies] = useState(new Set()); // Track failed movies
 
     useEffect(() => {
         const fetchMovieRecommendations = async () => {
@@ -28,6 +30,10 @@ export const Recommendation = ({ movieId }) => {
         }
     }, [movieId]);
 
+    const handleImageError = (movieId) => {
+        setFailedMovies((prevFailedMovies) => new Set(prevFailedMovies).add(movieId));
+    };
+
     return (
         <div className={styles.inside_box} data-theme={isDark ? "dark" : "light"}>
             {loading ? (
@@ -36,14 +42,16 @@ export const Recommendation = ({ movieId }) => {
                 <h1>{error}</h1>
             ) : recommendations && recommendations.length > 0 ? (
                 <div className={styles.movie_list}>
-                    {recommendations.map((movie) => (
-                        <div key={movie.id} className={styles.movie_item}>
-                            <Link to={`/Movie/${movie.id}`}>
-                                <img src={`https://image.tmdb.org/t/p/w138_and_h175_face${movie.backdrop_path}`} alt={movie.title} />
-                            </Link>
-                            {movie.title}
-                        </div>
-                    ))}
+                    {recommendations
+                        .filter(movie => !failedMovies.has(movie.id)) // Skip failed movies
+                        .map((movie) => (
+                            <div key={movie.id} className={styles.movie_item}>
+                                <Link to={`/Movie/${movie.id}`}>
+                                    <img src={`https://image.tmdb.org/t/p/w138_and_h175_face${movie.backdrop_path}`} alt={movie.title} onError={() => handleImageError(movie.id)} />
+                                </Link>
+                                {movie.title}
+                            </div>
+                        ))}
                 </div>
             ) : (
                 <h1>No Recommendations Available</h1>
@@ -51,3 +59,4 @@ export const Recommendation = ({ movieId }) => {
         </div>
     );
 };
+
