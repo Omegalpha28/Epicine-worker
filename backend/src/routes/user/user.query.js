8f2db9164aac56ddb5a7d58a47139ce47877398b
lagie-marin <marin.lagie@epitech.eu>
2024-10-04 09:55:03 +0200

    [UPDATE]: change place of the core folder in the root of the backend

--- backend/src/routes/user/user.query.js
+++ backend/src/routes/user/user.query.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { createUser, getUser, updateUser } = require("../../core/data/config.function");
+const { createUser, getUser, updateUser } = require("../../../core/data/config.function");
 const { error } = require("../../utils/Logger");
 
 module.exports = { checkAccountMail, register, getAccountMail }
