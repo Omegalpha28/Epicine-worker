f161a498536ab94cbe6db239d1d9073dcdc2cd89
Saravenpi Shigeturi <saravepi@tuta.io>
2024-10-04 14:56:44 +0200

    [ADD]: fonctionnality search bar on id page movies

--- frontend/src/components/MoviePage/MPage.jsx
+++ frontend/src/components/MoviePage/MPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useTheme from "../set_theme";
 import styles from "./MPage.module.css";
 import { Navbar } from "../Navbar/Navbar";
@@ -7,17 +8,25 @@ import { Join_Us } from "../Joinus/join_us";
 import { Movie_Profile } from "./Profile/movie_profile";
 import { useParams } from "react-router-dom";
 import { WallpaperMovie } from "./Wallpaper/WallpaperMovie";
+import { Search_Content } from "../Home/Search_Content/Search_Content";
 
 export const MPage = () => {
-
   const [isDark, setIsDark] = useTheme();
   const { id } = useParams();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchOpen, setSearchOpen] = useState(false);
 
   return (
     <div className={styles.page} data-theme={isDark ? "dark" : "light"}>
       <div className={app_styles.App}>
         <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
-        <Navbar />
+        <Navbar
+          setSearchQuery={setSearchQuery}
+          searchOpen={searchOpen}
+          setSearchOpen={setSearchOpen}
+        />
+        {searchOpen && searchQuery && <Search_Content query={searchQuery} />}
+
         <WallpaperMovie movieId={id} />
         <div className={styles.box}>
           <Movie_Profile movieId={id} />
