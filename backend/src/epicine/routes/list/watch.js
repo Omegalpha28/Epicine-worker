aca83c9f64c255623d7f72ab6ae177e9a32e93cc
lagie-marin <marin.lagie@epitech.eu>
2024-10-05 10:06:27 +0200

    [ADD]: added request to delete a WatchList to the API

--- backend/src/epicine/routes/list/watch.js
+++ backend/src/epicine/routes/list/watch.js
@@ -1,4 +1,4 @@
-const { getWatchList, addWatchList } = require("../../../../core/data/config.function");
+const { getWatchList, addWatchList, removeWatchList } = require("../../../../core/data/config.function");
 const auth = require("../../middleware/auth")
 
 module.exports = async function(client, app, bcrypt) {
@@ -12,6 +12,20 @@ module.exports = async function(client, app, bcrypt) {
                 res.status(404).json({"msg": "Internal server error"});
             else
                 res.status(200).json({"msg": "added"});
+        }
+        else res.status(404).json({"msg": "Internal server error"});
+    });
+
+    app.delete("/remove/watchlist", auth, async (req, res) => {
+        const uuid = req.body["uuid"];
+        const film_id = req.body["film_id"];
+        const watchData = (await getWatchList(client, {userUUID: uuid, film_id: film_id})).data;
+
+        if (watchData) {
+            if (!(await removeWatchList(client, uuid, film_id)))
+                res.status(404).json({"msg": "Internal server error"});
+            else
+                res.status(200).json({"msg": "removed"});
         }
         else res.status(404).json({"msg": "Internal server error"});
     })
