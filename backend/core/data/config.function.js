40265a75349c24032676aa2609ea735255910504
lagie-marin <marin.lagie@epitech.eu>
2024-10-05 09:56:04 +0200

    [UPDATE]: added functions to call WatchList queries

--- backend/core/data/config.function.js
+++ backend/core/data/config.function.js
@@ -1,8 +1,11 @@
-module.exports = {getUser, createUser, updateUser, getFavorite, addFavorite, removeFavorite};
+module.exports = {getUser, createUser, updateUser, getFavorite, addFavorite, removeFavorite, getWatchList, addWatchList, removeWatchList};
 
 async function getUser(client, userInfo){return await client.getUser(userInfo);}
 async function createUser(client, name, email, mdp){return await client.createUser(name, email, mdp);}
 async function updateUser(client, uuid, settings){return await client.updateUser(uuid, settings);}
-async function getFavorite(client, favInfo){return client.getFavorite(favInfo);}
-async function addFavorite(client, uuid, film_id){return client.addFavorite(uuid, film_id);}
-async function removeFavorite(client, uuid, film_id){return client.removeFavorite(uuid, film_id);}
+async function getFavorite(client, favInfo){return await client.getFavorite(favInfo);}
+async function addFavorite(client, uuid, film_id){return await client.addFavorite(uuid, film_id);}
+async function removeFavorite(client, uuid, film_id){return await client.removeFavorite(uuid, film_id);}
+async function getWatchList(client, watchInfo){return await client.getWatchList(watchInfo);}
+async function addWatchList(client, uuid, film_id){return await client.addWatchList(uuid, film_id);}
+async function removeWatchList(client, uuid, film_id){return await client.removeWatchList(uuid, film_id);}
