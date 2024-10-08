65f9d34bcabfc5bebd9cdc5977c185c1f3aca189
lagie-marin <marin.lagie@epitech.eu>
2024-10-08 12:13:00 +0200

    [ADD]: get fil popular

---
+++ backend/src/forum/routes/fil/fil.js
@@ -0,0 +1,11 @@
+const { Fil } = require("../../../../core/data/models");
+const auth = require("../../../epicine/middleware/auth");
+
+module.exports = async function(client, app, bcrypt) {
+    app.get("/get/fil/popular", async (req, res) => {
+        FilData = await Fil.customRequest("SELECT Fil.id,Fil.titre,Fil.description,SUM(IFNULL(Likes.type,0)) AS somme FROM Message LEFT Join Likes ON Likes.id_message=Message.id JOIN Fil ON Fil.id=Message.id_fil GROUP BY Fil.id ORDER BY somme DESC LIMIT 10");
+
+        if (FilData) res.status(200).json(FilData);
+        else res.status(404).json({ "msg": "No popular movies found" });
+    });
+}
