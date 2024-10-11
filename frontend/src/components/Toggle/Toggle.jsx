83674f10badb76a800f24eb02463207524ad84b4
Saravenpi Shigeturi <saravepi@tuta.io>
2024-10-11 17:26:05 +0200

    [ADD]: rated movies + add toggle theme button in preference settings

--- frontend/src/components/Toggle/Toggle.jsx
+++ frontend/src/components/Toggle/Toggle.jsx
@@ -3,14 +3,12 @@ import "./Toggle.css";
 export const Toggle = ({ handleChange, isChecked }) => {
   return (
     <div className="toggle-container">
-      <input
-        type="checkbox"
-        id="check"
-        className="toggle"
-        onChange={handleChange}
-        checked={isChecked}
-      />
-      <label htmlFor="check">Dark Mode</label>
+      <button
+        className={`toggle-button ${isChecked ? "dark" : "light"}`}
+        onClick={handleChange}
+      >
+        {isChecked ? "Dark Theme" : "Light Theme"}
+      </button>
     </div>
   );
 };
