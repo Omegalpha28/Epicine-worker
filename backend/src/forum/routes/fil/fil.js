fd314fd5acff47b1ce950460a73f268e2212e3c0
lagie-marin <marin.lagie@epitech.eu>
2024-10-11 14:16:10 +0200

    [UPDATE]: move get fil popular in the file fil_popular.js

--- backend/src/forum/routes/fil/fil.js
+++ backend/src/forum/routes/fil/fil.js
@@ -4,18 +4,6 @@ const auth = require("../../../epicine/middleware/auth");
 const { error } = require("../../../utils/Logger");
 
 module.exports = async function(client, app, bcrypt) {
-    app.get("/get/fil/popular", async (req, res) => {
-        var filData = (await Fil.customRequest("SELECT Fil.id,Fil.title,Fil.description,SUM(IFNULL(Likes.type,0)) AS somme FROM Message LEFT Join Likes ON Likes.id_message=Message.id JOIN Fil ON Fil.id=Message.id_fil GROUP BY Fil.id ORDER BY somme DESC LIMIT 10")).data;
-
-        if (filData.length != 0) res.status(200).json(filData);
-        else {
-            filData = (await Fil.customRequest("SELECT Fil.id,Fil.title,Fil.description FROM Fil LIMIT 10")).data;
-
-            if (filData.length != 0) res.status(200).json(filData);
-            else res.status(404).json({ "msg": "No popular movies found" });
-        }
-    });
-
     app.post("/add/fil", auth, async (req, res) => {
         const { title, film_id, description } = req.body;
         const filData = (await Fil.findOne({title: title})).data;
