346288db0492b9c68a77d28514126e887e656171
lagie-marin <marin.lagie@epitech.eu>
2024-10-16 22:31:40 +0200

    [ADD]: delete user request

--- backend/src/epicine/routes/user/user.query.js
+++ backend/src/epicine/routes/user/user.query.js
@@ -2,8 +2,6 @@ const jwt = require("jsonwebtoken");
 const { createUser, getUser, updateUser } = require("../../../../core/data/config.function");
 const { error } = require("../../../utils/Logger");
 
-module.exports = { checkAccountMail, register, getAccountMail }
-
 async function checkAccountMail(client, res, email, callback)
 {
     const userData = await getUser(client, {email: email});
@@ -11,7 +9,7 @@ async function checkAccountMail(client, res, email, callback)
     if (userData.data == undefined)
         callback(0);
     else
-        callback(84);
+    callback(84);
 }
 
 async function register(client, res, email, name, mdp)
@@ -19,7 +17,7 @@ async function register(client, res, email, name, mdp)
     try {
         const result = await createUser(client, name, email, mdp);
         const token = jwt.sign({uuid: result.uuid}, process.env.SECRET);
-
+        
         res.status(201).json({token: token});
     } catch (err) {
         error(err);
@@ -46,3 +44,19 @@ async function getAccountMail(client, res, email, mdp, bcrypt, callback)
             callback(84);
     }
 }
+
+async function checkMdp(client, res, email, mdp)
+{
+    const userData = (await getUser(client, {email: email})).data;
+
+    if (userData == undefined)
+        return 0;
+    var mdp2 = userData.password;
+
+    if (bcrypt.compareSync(mdp, mdp2))
+        return 1;
+    else
+        return 0;
+}
+
+module.exports = { checkAccountMail, register, getAccountMail, checkMdp }
