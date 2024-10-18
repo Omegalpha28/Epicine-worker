f92c6541970fe67af1641ddb83ee4011e78c9f53
lagie-marin <marin.lagie@epitech.eu>
2024-10-18 16:11:02 +0200

    [ADD]: update publication request

--- backend/src/epicine/routes/events/publication/publication.js
+++ backend/src/epicine/routes/events/publication/publication.js
@@ -39,5 +39,30 @@ module.exports = async function(client, app, bcrypt) {
             res.status(404).json({"msg": "Publication not found"});
         else
             res.status(200).json(publications);
+    });
+    app.post("/update/publication", auth, async (req, res) => {
+        const uuid = req.uuiduser;
+        const {id, title, text} = req.body;
+        const userData = (await getUser(client, {uuid: uuid})).data;
+
+        if (userData.status != 1) {
+            res.status(401).json({"msg": "Unauthorized"});
+            return;
+        }
+        if (title == undefined || text == undefined) {
+            res.status(400).json({"msg": "title and text is required"});
+            return;
+        }
+        if (title.length == 0 || text.length == 0) {
+            res.status(400).json({"msg": "title and text is required"});
+            return;
+        }
+        const pubData = (await Publication.findOne({id: id, auteur: uuid}));
+        if (pubData.data != undefined)
+            delete pubData.data.date;
+        if (await pubData.updateOne({title: title, text: text}))
+            res.status(200).json({"msg": "Publication updated"});
+        else
+            res.status(500).json({"msg": "Internal server error"});
     })
 }
